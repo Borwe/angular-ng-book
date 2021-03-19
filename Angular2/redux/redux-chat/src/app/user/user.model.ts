8392c4e9311316c68cb8cec70c578635a46aace5
brian.orwe <brian.orwe@gmail.com>
2021-03-19 17:00:58 +0300

    Remove physical Angular2 file to use git repo

--- Angular2/redux/redux-chat/src/app/user/user.model.ts
+++
@@ -1,9 +0,0 @@
-/**
- * A User represents an agent that sends messages
- */
-export interface User {
-  id: string;
-  name: string;
-  avatarSrc: string;
-  isClient?: boolean;
-}
--- Angular2/upgrade/hybrid/src/typings.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="./js/app.d.ts"/>
-
-/* SystemJS module definition */
-declare var module: {
-  id: string;
-};
